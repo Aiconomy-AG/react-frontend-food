33788047081e4fda49c2deeefa4efe94b17c61e8
Baicu Stefan <stefan.baicu@stud.ubbcluj.ro>
2026-07-02 14:11:59 +0300

    a

--- src/services/api.js
+++ src/services/api.js
@@ -5,9 +5,8 @@ const API = axios.create({
 });
 
 export const recipeService = {
-    getAll: () => API.get('').then(res => res.data),
-    create: (data) => API.post('', data).then(res => res.data),
-    delete: (id) => API.delete(`/${id}`).then(res => res.data),
-    getRandom: () => API.get('/random-recipe').then(res => res.data), 
-
+    getAll: () => API.get('/recipes').then(res => res.data),
+    create: (data) => API.post('/recipes', data).then(res => res.data),
+    delete: (id) => API.delete(`/recipes/${id}`).then(res => res.data),
+    getRandom: () => API.get('/recipes/random-recipe').then(res => res.data),
 };
